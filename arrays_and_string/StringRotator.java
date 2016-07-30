02b0e9458c08bf4ce519a2110f38330d72ab0a85
izzabudaka <izz.abudaka@gmail.com>
2016-07-31 00:51:16 +0100

    string rotator

--- arrays_and_string/StringRotator.java
+++ arrays_and_string/StringRotator.java
@@ -1,22 +1,8 @@
 public class StringRotator{
   public boolean isRotation(String s1, String s2){
-    if(s1.length() != s2.length())
+    if(s1.length() != s2.length() || s2.length == 0)
         return false;
-
-    int i = 0;
-    while( i < s2.length()) {
-      while(i < s2.length() && s1.charAt(0) != s2.charAt(i))
-        i++;
-      
-      int rotationPoint = i;
-      while(i < s2.length() && s1.charAt(0) == s2.charAt(i))
-        i++;
-      
-      if(i == s2.length())
-        return isSubstring(s2.substring(0, rotationPoint), s1);
-      else
-        i = rotationPoint + 1;
-    }
-    return false;
+  
+    return isSubtring(s1 + s1, s2);
   }
 }
