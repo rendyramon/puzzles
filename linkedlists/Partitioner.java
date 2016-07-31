40c857e1f701a3cd7169f5692f7c6062cfb90cea
izzabudaka <izz.abudaka@gmail.com>
2016-07-31 12:04:43 +0100

    Partitioner

---
+++ linkedlists/Partitioner.java
@@ -0,0 +1,51 @@
+public class Partitioner{
+  public static class Node {
+    public Node next = null;
+    public int data;
+
+    public Node(int data){
+      this.data = data;
+    }
+
+    public void print(){
+      System.out.print("--->" + data);
+      if(next != null)
+        next.print();
+      else
+        System.out.println();
+    }
+  }
+
+  public void partition(Node node, int value){
+    boolean encounteredVal = false;
+    Node head = node;
+    if(head.data == value)
+      encounteredVal = true;
+
+    while(head != null && head.next != null){
+      if(encounteredVal && head.next.data < value) {
+        // move smaller element to the begining
+        Node next = head.next;
+        head.next = head.next.next;
+        next.next = node;
+        node = next;
+      } else if(head.next.data == value){
+          encounteredVal = true;
+          head = head.next;
+      }
+    }
+  }
+
+  public static void main(String[] args){
+    Node node = new Node(1);
+    node.next = new Node(5);
+    node.next.next = new Node(3);
+    node.next.next.next = new Node(4);
+
+    node.print();
+
+    new Partitioner().partition(node, 5);
+
+    node.print(); 
+  }
+}
